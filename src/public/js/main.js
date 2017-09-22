fe99c84c845ac20af46e32453d81490a1145a3d9
Yosnier Samon M <yosamac@gmail.com>
2017-09-22 02:46:18 +0200

    Added services layer

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,2 +1,12 @@
 window.$ = window.jQuery = require("jquery");
 
+import InstrumentsService from "./InstrumentsService";
+
+const instrumentsService = new InstrumentsService("/instruments/");
+
+
+instrumentsService.list(instruments => {
+    console.log(instruments);
+}, error => {
+    console.log(error);
+});
